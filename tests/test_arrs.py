9e47cc103cb93686ef2ce767e4d4ebcac580201f
maria <maria.vyukhina@mail.ru>
2023-04-28 14:42:17 +0400

    Adding tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -6,13 +6,40 @@ class TestArrs(unittest.TestCase):
 
     def test_get(self):
         self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
+        self.assertEqual(arrs.get([1, 2, 3, 4], -1, 'test'), 'test')
 
         with self.assertRaises(IndexError):
-            self.assertEqual(arrs.get([], 0, "test"))
+            arrs.get([], 0, "test")
+
+        with self.assertRaises(IndexError):
+            arrs.get([1, 2, 3, 4], 18, "test")
+
+        with self.assertRaises(TypeError):
+            arrs.get([1, 2, 3], '2', "test")
 
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(arrs.my_slice(['1', '2', '3'], 2, 6), ['3'])
+        self.assertEqual(arrs.my_slice([], 2, 6), [])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], -6, -6), [])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4]), [1, 2, 3, 4])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 6, -6), [])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], -1, 6), [4])
+        self.assertEqual(arrs.my_slice([1, 2, 3], -1), [3])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 6, 6), [])
+
+        with self.assertRaises(TypeError):
+            arrs.my_slice(['1', '2', '3'], 2, '6')
+
+        with self.assertRaises(TypeError):
+            arrs.my_slice([1, 2, 3], '2', 6)
+
+        with self.assertRaises(TypeError):
+            arrs.my_slice([1, 2, 3], '2', '6')
+
+        with self.assertRaises(TypeError):
+            arrs.my_slice({1, 2, 3}, 2, 6)
 
 
 if __name__ == '__main__':
